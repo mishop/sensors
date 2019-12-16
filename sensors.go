0a03f794bf5fe523c4ff910b04f536aed071b99f
Misho M. Petkovic <mishop@hotmail.com>
2019-12-16 22:28:07 +0100

    Update sensors.go

--- sensors.go
+++ sensors.go
@@ -14,12 +14,10 @@ func main() {
 
     fmt.Printf("w1_temp:\n")
 
-    for _, sensor := range sensors {
-	i := 1
+    for i, sensor := range sensors {
         t, err := ds18b20.Temperature(sensor)
         if err == nil {
 		fmt.Printf("temp%v \n temp%v_output:%.2f \n", i, i, t)
         }
-	i =i+1
     }
 }
