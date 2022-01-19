0d49eb741de42d25f85730c2857645b85d2149c9
Misho M. Petkovic <mishop@hotmail.com>
2022-01-19 03:09:06 +0100

    Update sensors.go
    removed external package

--- sensors.go
+++ sensors.go
@@ -2,12 +2,58 @@ package main
 
 import (
     "fmt"
-
-    "github.com/yryz/ds18b20"
+    "errors"
+    "io/ioutil"
+    "strconv"
+    "strings"
 )
 
+var ErrReadSensor = errors.New("failed to read sensor temperature")
+
+// Sensors get all connected sensor IDs as array
+func Sensors() ([]string, error) {
+    data, err := ioutil.ReadFile("/sys/bus/w1/devices/w1_bus_master1/w1_master_slaves")
+    if err != nil {
+        return nil, err
+    }
+
+    sensors := strings.Split(string(data), "\n")
+    if len(sensors) > 0 {
+        sensors = sensors[:len(sensors)-1]
+    }
+
+    return sensors, nil
+}
+
+// Temperature get the temperature of a given sensor
+func Temperature(sensor string) (float64, error) {
+    data, err := ioutil.ReadFile("/sys/bus/w1/devices/" + sensor + "/w1_slave")
+    if err != nil {
+        return 0.0, ErrReadSensor
+    }
+
+    raw := string(data)
+
+    if !strings.Contains(raw, " YES") {
+        return 0.0, ErrReadSensor
+    }
+
+    i := strings.LastIndex(raw, "t=")
+    if i == -1 {
+        return 0.0, ErrReadSensor
+    }
+
+    c, err := strconv.ParseFloat(raw[i+2:len(raw)-1], 64)
+    if err != nil {
+        return 0.0, ErrReadSensor
+    }
+
+    return c / 1000.0, nil
+}
+
+
 func main() {
-    sensors, err := ds18b20.Sensors()
+    sensors, err := Sensors()
     if err != nil {
         panic(err)
     }
@@ -15,9 +61,9 @@ func main() {
     fmt.Printf("w1_temp:\n")
 
     for i, sensor := range sensors {
-        t, err := ds18b20.Temperature(sensor)
+        t, err := Temperature(sensor)
         if err == nil {
-		fmt.Printf("temp%v \n temp%v_output:%.2f \n", i, i, t)
+		fmt.Printf("temp%v: \n  temp%v_input: %.2f \n", i, i, t)
         }
     }
 }
